5c493b9eb1eeabb3ef05dfcd6531128f5db9ecc9
Neil Vallon <neilvallon@gmail.com>
2014-05-13 07:08:05 -0500

    Model networked player
    Mimics network delayed position tick from player 1

--- lagTest.elm
+++ lagTest.elm
@@ -4,14 +4,25 @@ time = lift (inSeconds . fst) (timestamp (fps 30))
 
 rotate r = (\t -> ((sin t * r), (cos t * r))) <~ time
 
+discrete sig = sampleOn (fps 4) sig
+delayed sig = delay (200 * millisecond) sig
+
 player color controller =
   let
     c = filled color (circle 5)
     m cord = move cord c
   in m <~ controller
 
-player1 = player red (rotate 50)
+-- Local Player
+p1Control = rotate 50
+player1 = player red p1Control
+--
 
-render (w, h) p1 = collage w h [p1]
+-- Network Player
+p2Control = discrete (delayed p1Control)
+player2 = player blue p2Control
+--
 
-main = render <~ Window.dimensions ~ player1
+render (w, h) p1 p2 = collage w h [p1, p2]
+
+main = render <~ Window.dimensions ~ player1 ~ player2
